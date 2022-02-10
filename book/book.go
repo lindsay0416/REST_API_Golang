deecd71bafaa1485ac76c0cad98534819aa0f276
lindsay0416 <lindsay.z@zhukeke.co.nz>
2022-02-10 21:44:44 +1300

    REST API Golang

---
+++ book/book.go
@@ -0,0 +1,53 @@
+package book
+
+import (
+	"github.com/database"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+)
+
+type Book struct {
+	gorm.Model
+	Title  string `json:"name"`
+	Author string `json:"author"`
+	Rating int    `json:"rating"`
+}
+
+func GetBooks(c *fiber.Ctx) {
+	db := database.DBConn
+	var books []Book
+	db.Find(&books)
+	c.JSON(books)
+}
+
+func GetBook(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBConn
+	var book Book
+	db.Find(&book, id)
+	c.JSON(book)
+}
+
+func NewBook(c *fiber.Ctx) {
+	db := database.DBConn
+	var book Book
+	book.Title = "1984"
+	book.Author = "George Orwell"
+	book.Rating = 5
+	db.Create(&book)
+	c.JSON(book)
+}
+
+func DeleteBook(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBConn
+
+	var book Book
+	db.First(&book, id)
+	if book.Title == "" {
+		c.Status(500).Send("No Book Found with ID")
+		return
+	}
+	db.Delete(&book)
+	c.Send("Book Successfully deleted")
+}
